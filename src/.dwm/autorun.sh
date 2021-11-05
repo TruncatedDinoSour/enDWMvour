c803500fa0dc4f63a656baecd78c01cc42b8abaa
Ari Archer <truncateddinosour@gmail.com>
2021-11-06 00:50:35 +0200

    locker

--- src/.dwm/autorun.sh
+++ src/.dwm/autorun.sh
@@ -1,4 +1,5 @@
 bdwmb -q &
 feh --bg-fill /usr/share/backgrounds/dwm.png &
 [[ ! -f ~/.dwm/.docs_lock ]] && (st -e ~/.dwm/scripts/docs.sh; touch ~/.dwm/.docs_lock) &
+xautolock -time 10 -locker /usr/local/bin/dwmlocker &
 
